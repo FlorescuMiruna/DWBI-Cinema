69ef1514968b5735650321581cca1f9a80156e5c
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2024-01-31 20:18:01 +0200

    Added cinema

--- cinema/src/main/java/com/example/cinema/model/Cinema.java
+++ cinema/src/main/java/com/example/cinema/model/Cinema.java
@@ -1,4 +1,30 @@
 package com.example.cinema.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cinema")
 public class Cinema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Integer id;
+    private String nume;
+    private Integer nrLocuri;
+    private Integer nrSali;
+    private Integer nrOras;
+
 }
---
+++ cinema/src/main/java/com/example/cinema/service/CinemaService.java
@@ -0,0 +1,25 @@
+package com.example.cinema.service;
+
+import com.example.cinema.model.Cinema;
+import com.example.cinema.repository.CinemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CinemaService {
+
+    private final CinemaRepository cinemaRepository;
+
+    @Autowired
+    public CinemaService(CinemaRepository cinemaRepository) {
+        this.cinemaRepository = cinemaRepository;
+    }
+
+    public List<Cinema> getAllCinemas() {
+        return cinemaRepository.findAll();
+    }
+
+    public Cinema addCinema(Cinema cinema) {
+        return cinemaRepository.save(cinema);
+    }
+}
